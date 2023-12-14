781e557773d0bf8a22cdb0df70d9e1332698d62a
jhanca-vm <jhanca-vm@proton.me>
2023-12-13 20:46:52 -0500

    fix: Improve the solution to challenge #9

--- challenges/9.test.ts
+++ challenges/9.test.ts
@@ -1,15 +1,19 @@
 import { assertEquals } from 'assert'
 
 function adjustLights(lights: string[]) {
+  const colors = ['🟢', '🔴']
+
+  colors[1] = colors[+(lights[0] === '🟢')]
+  colors[0] = lights[0]
+
+  let index = 0
   let count = 0
-  let previous = ''
 
   for (const light of lights) {
-    count += +(light === previous)
-    previous = [light, ''][+(light === previous)]
+    count += +(light !== colors[+(index++ % 2)])
   }
 
-  return count
+  return Math.min(lights.length - count, count)
 }
 
 Deno.test('Reto #9: 🚦 Alterna las luces', () => {
